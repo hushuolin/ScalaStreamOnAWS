e9950d320a4090c64ebaaa1c14f23d031ab991cb
StarDasToDoRaGon <zhixin.zhang@alumni.stonybrook.edu>
2024-04-18 07:43:46 -0400

    finished data analysis.

--- src/main/scala/dataAnalysis/transformAndAnalysis.scala
+++ src/main/scala/dataAnalysis/transformAndAnalysis.scala
@@ -4,30 +4,24 @@ import org.apache.spark.sql.SparkSession
 
 object transformAndAnalysis {
   def main(args: Array[String]): Unit = {
-    // 初始化 Spark 会话
     val spark = SparkSession.builder
       .appName("Convert Parquet to CSV")
-      .config("spark.master", "local") // 可以更改为 "spark://master:port" 用于集群运行
+      .config("spark.master", "local")
       .getOrCreate()
 
-    // 设定日志级别
     spark.sparkContext.setLogLevel("WARN")
 
-    // 读取 Parquet 文件
     val parquetFile = "src/main/resources/data_19601.parquet"
     val dataFrame = spark.read.parquet(parquetFile)
 
-    // 数据处理，这里仅仅是简单地显示前几行以便调试
     dataFrame.show()
 
-    // 将数据写入 CSV 文件
     val csvOutput = "src/main/resources/output/csvOutput"
     dataFrame.write
-      .option("header", "true") // 在 CSV 文件中包含头部信息
-      .mode("overwrite") // 添加这行代码以覆盖现有文件
+      .option("header", "true")
+      .mode("overwrite")
       .csv(csvOutput)
 
-    // 停止 Spark 会话
     spark.stop()
   }
 
