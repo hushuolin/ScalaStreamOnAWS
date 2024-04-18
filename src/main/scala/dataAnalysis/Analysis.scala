46d9c01d0d132361aaa1de56a2097c55497d134a
StarDasToDoRaGon <zhixin.zhang@alumni.stonybrook.edu>
2024-04-18 08:03:43 -0400

    delete the unnecessary comment

--- src/main/scala/dataAnalysis/Analysis.scala
+++ src/main/scala/dataAnalysis/Analysis.scala
@@ -7,21 +7,17 @@ object Analysis {
       .master("local[*]") // 或者设置你的集群配置
       .getOrCreate()
 
-    // 读取 CSV 文件
     val df = spark.read
       .option("header", "true")
       .option("inferSchema", "true") // 自动推断字段类型
       .csv("src/main/resources/output/csvOutput/part-00000-c6c3fb30-b914-4214-9ac7-49f9e858fc9d-c000.csv")
 
-    // 执行数据分析, 例如计算每个公司的投诉总数
     val analysisResult = df.groupBy("company")
       .agg(F.count("complaint_id").alias("total_complaints"))
       .orderBy(F.col("total_complaints").desc)
 
-    // 展示结果（可选）
     analysisResult.show()
 
-    // 将结果保存为新的 CSV 文件
     analysisResult.coalesce(1) // 如果结果不大，可以合并为一个文件
       .write
       .option("header", "true")
